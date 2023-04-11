18bc3a43f73fcfebf8e33c2fb4183a0f43273cfd
Saif Maamun <sadifmamun@gmail.com>
2023-04-11 09:38:38 +0600

    form

--- src/components/AddUsers/AddUsers.js
+++ src/components/AddUsers/AddUsers.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
 const AddUsers = () => {
+
+
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.pass.value;
+        const address = e.target.address.value;
+        const user = {name: name, email: email, password: password, address: address}
+        console.log(user)
+
+
+        e.target.reset()
+    }
     return (
         <div>
             <h2>
                 Add users
             </h2>
+                <h3>forms</h3>
+            <form onSubmit={handleSubmit} >
+                <input type="text" name="name" id="" placeholder='enter your name' /><br />
+                <input type="email" name="email" id="" placeholder='enter your mail' /><br />
+                <input type="password" name="pass" id=""placeholder='Type your pasword'  /><br />
+                <input type="text" name="address" id="" placeholder='enter your full address' /><br />
+                <button type="submit">Add User</button>
+            </form>
         </div>
     );
 };
