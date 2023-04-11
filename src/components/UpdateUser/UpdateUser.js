950f9d78cdca3959fc694bf4a907672681f160dd
Saif Maamun <sadifmamun@gmail.com>
2023-04-11 18:50:42 +0600

    updating user via form

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,12 +1,44 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const UpdateUser = () => {
     const user  = useLoaderData();
     const [userData,setUsersData] = useState(user);
+
+
+
+
+    // update user on submit
+    const updateUser=e=>{
+        e.preventDefault();
+        fetch()
+
+    }
+
+    const getData=e=>{
+        const field= e.target.name;
+        const data = e.target.value;
+        const updatedUser={...user}
+        updatedUser[field] = data;
+        setUsersData(updatedUser)
+    }
+
+
+
     return (
         <div>
-            <h2>updating User {user.name}</h2>
+            <Link to={'/'}> <button>Home</button></Link>
+            <Link to={'/add'}> <button>Add Users</button></Link>
+            <h2>Updating User {user.name}</h2>
+
+            <div>   
+                <form onSubmit={updateUser}>
+                    <input onChange={getData} type="text" name="name" id="" defaultValue={user.name} /><br />
+                    <input onChange={getData} type="email" name="email" id="" defaultValue={user.email} /><br />
+                    <input onChange={getData} type="text" name="address" id="" defaultValue={user.address} /><br />
+                    <button type="submit">Update</button>
+                </form>
+            </div>
         </div>
     );
 };
