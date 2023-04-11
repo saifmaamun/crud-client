33ad27f15388a62338d18d85030aede5ee3c3791
Saif Maamun <sadifmamun@gmail.com>
2023-04-11 11:13:36 +0600

    deleteing data from  client side

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,21 @@ import { useLoaderData } from 'react-router-dom';
 const Home = () => {
     const users  = useLoaderData();
     const [usersData,setUsersData] = useState(users);
-    console.log(users)
+
+    const handleDelete=user=>{
+        const permission=window.confirm(`Are you sure you want to delete {user.name}`)
+        if (permission){
+            fetch(`http://localhost:7000/user/${user._id}`,{
+            method:"DELETE",
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+            })
+        }
+    }
+
+
     return (
         <div>
             <h2>
@@ -14,6 +28,7 @@ const Home = () => {
                 usersData.map(user=>
                     <div key={user._id}>
                         <p>{user._id}, {user.name}, {user.email}, {user.address}</p>
+                        <button onClick={()=>handleDelete(user)}> delete user</button>
                         <br />
                         <br />
                     </div>
